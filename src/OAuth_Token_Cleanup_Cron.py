39393e6ed7be990bf8ec430ef9b2825795c1a44e
Mono Chong <yusha9788@gmail.com>
2024-06-25 10:14:35 +0800

    fix AttributeError: 'dict' object has no attribute 'iteritems'

--- src/OAuth_Token_Cleanup_Cron.py
+++ src/OAuth_Token_Cleanup_Cron.py
@@ -9,11 +9,10 @@ import uuid
 import socket
 from email.mime.text import MIMEText
 import sys
-import argparse
 import os.path
 
 # Loads hostname of server to give detail in notification email
-casshost=socket.gethostname()
+casshost = socket.gethostname()
 
 # Parses Arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -37,58 +36,66 @@ parser.add_argument('--ca', type=str, help='If using SSL, provide a path to the 
 
 args = parser.parse_args()
 if args.ssl:
-        if args.ca is None:
-                parser.error("--ssl requires --ca to set a truststore")
-        if (not os.path.exists(args.ca)):
-                parser.error("--ca file not found or reachable")
-        ssl_opts = {
-            'ca_certs': args.ca,
-            'ssl_version': PROTOCOL_TLSv1,
-            'cert_reqs': CERT_REQUIRED  # Certificates are required and validated
-        }
+    if args.ca is None:
+        parser.error("--ssl requires --ca to set a truststore")
+    if not os.path.exists(args.ca):
+        parser.error("--ca file not found or reachable")
+    ssl_opts = {
+        'ca_certs': args.ca,
+        'ssl_version': PROTOCOL_TLSv1,
+        'cert_reqs': CERT_REQUIRED  # Certificates are required and validated
+    }
 else:
-        ssl_opts = {}
+    ssl_opts = {}
 
 
 def sendEmailAlert(BODY, dbhost, subject):
-        msg = MIMEText(BODY, 'html')
-        msg['Subject'] = dbhost + " " + subject
-        sender = dbhost + " OAuth Token Cleanup Script"
-        recipients = [args.email]
-        msg['From'] = sender
-        msg['To'] = ", ".join(recipients)
-        server = smtplib.SMTP(args.smtpserver)
-        server.sendmail(sender, recipients, msg.as_string())
-        server.quit()
-def deleteExpiredIDs(host,keyspace,user,password):
-        # subtracts 7200 because we want to only delete those tokens more than two hours old. Subtacts 21600 (6 hours) because cassandra compensates for the UTC/CTC time difference
-        two_hours_ago = datetime.datetime.utcfromtimestamp(int(time.time())- 7200 - 21600)
-        auth_provider = PlainTextAuthProvider(username=user, password=password)
-        cluster = Cluster([host],ssl_options=ssl_opts,auth_provider=auth_provider,port=9042)
-        session = cluster.connect(keyspace)
-        str_two_hours_ago = str(two_hours_ago)
-        rows = session.execute("SELECT id, credential_id FROM oauth2_tokens where created_at <='" + str_two_hours_ago  + "' ALLOW FILTERING")
-        rowsdeleted = 0
-        consumer_tokens = {}
-        for token_row in rows:
-                 # Used to determine is a consumer is abusing token service
-                if token_row.credential_id in consumer_tokens:
-                        consumer_tokens[token_row.credential_id] += 1
-                else:
-                        consumer_tokens[token_row.credential_id] = 1
-                rowsdeleted+=1
-                session.execute("DELETE from oauth2_tokens where id=" + str(token_row.id) + "")
-
-        consumer_abuse_table = "<hr/><span class=\"black\">Consumer Token Creation Abuse (If any): </span><hr/>"
-        for key, value in consumer_tokens.iteritems():
-                if value >= 100:
-                        offending_creds = session.execute("SELECT consumer_id FROM oauth2_credentials where id = " + str(key).replace('(','').replace(')','') +  " ALLOW FILTERING")
-                        offending_consumer_id = str(offending_creds[0].consumer_id)
-                        offending_consumer = session.execute("SELECT username FROM consumers where id = " + offending_consumer_id + " ALLOW FILTERING")
-                        offending_consumer_username = str(offending_consumer[0].username)
-                        consumer_abuse_table+= "<br/><span class=\"black\">Cassandra Keyspace: </span> " + str(keyspace)+                               "<br/><span class=\"black\">Consumer ID: </span> "+ offending_consumer_id +                         "<br/><span class=\"black\">Consumer Name: </span> "+ offending_consumer_username +                              "<br/><span class=\"black\">Tokens Created: </span> "+ str(value) + "<hr/>"
+    msg = MIMEText(BODY, 'html')
+    msg['Subject'] = dbhost + " " + subject
+    sender = dbhost + " OAuth Token Cleanup Script"
+    recipients = [args.email]
+    msg['From'] = sender
+    msg['To'] = ", ".join(recipients)
+    server = smtplib.SMTP(args.smtpserver)
+    server.sendmail(sender, recipients, msg.as_string())
+    server.quit()
 
 
-        sendEmailAlert("<html> <head> <style> body{ font-family: FrutigerLTStd-Light, Arial, sans-serif;} h1, h2, h3, h4 { color:#e87722; font-weight: bold; text-rendering: optimizeLegibility; margin: 0 0 24px 0; } .black{ color:#333333; }.light{ color:#8c8c8c; font-size: 10px; } </style> </head> <body> <br/> <h1><span class=\"black\">Server:</span> " + casshost + "<br/><span class=\"black\">Cassandra Keyspace: </span> "+str(keyspace) +"<br/><span class=\"black\">OAuth_Tokens Rows Deleted: </span> "+str(rowsdeleted)+"</h1> "+str(consumer_abuse_table)+"</body> </html>",casshost,"OAuth Token Cleanup")
-        cluster.shutdown()
+def deleteExpiredIDs(host, keyspace, user, password):
+    # subtracts 7200 because we want to only delete those tokens more than two hours old. Subtracts 21600 (6 hours) because cassandra compensates for the UTC/CTC time difference
+    two_hours_ago = datetime.datetime.utcfromtimestamp(int(time.time()) - 7200 - 21600)
+    auth_provider = PlainTextAuthProvider(username=user, password=password)
+    cluster = Cluster([host], ssl_options=ssl_opts, auth_provider=auth_provider, port=9042)
+    session = cluster.connect(keyspace)
+    str_two_hours_ago = str(two_hours_ago)
+    rows = session.execute("SELECT id, credential_id FROM oauth2_tokens WHERE created_at <= %s ALLOW FILTERING", [str_two_hours_ago])
+    rowsdeleted = 0
+    consumer_tokens = {}
+    for token_row in rows:
+        # Used to determine if a consumer is abusing token service
+        if token_row.credential_id in consumer_tokens:
+            consumer_tokens[token_row.credential_id] += 1
+        else:
+            consumer_tokens[token_row.credential_id] = 1
+        print(f"Deleting token with ID: {token_row.id} and Credential ID: {token_row.credential_id}")
+        rowsdeleted += 1
+        session.execute("DELETE FROM oauth2_tokens WHERE id = %s", [str(token_row.id)])
+
+    consumer_abuse_table = "<hr/><span class=\"black\">Consumer Token Creation Abuse (If any): </span><hr/>"
+    for key, value in consumer_tokens.items():
+        if value >= 100:
+            offending_creds = session.execute("SELECT consumer_id FROM oauth2_credentials WHERE id = %s ALLOW FILTERING", [str(key)])
+            offending_consumer_id = str(offending_creds[0].consumer_id)
+            offending_consumer = session.execute("SELECT username FROM consumers WHERE id = %s ALLOW FILTERING", [offending_consumer_id])
+            offending_consumer_username = str(offending_consumer[0].username)
+            print(f"Cassandra Keyspace: {keyspace}, Consumer ID: {offending_consumer_id}, Consumer Name: {offending_consumer_username}, Tokens Created: {value}")
+            consumer_abuse_table += "<br/><span class=\"black\">Cassandra Keyspace: </span> " + str(keyspace) + \
+                                    "<br/><span class=\"black\">Consumer ID: </span> " + offending_consumer_id + \
+                                    "<br/><span class=\"black\">Consumer Name: </span> " + offending_consumer_username + \
+                                    "<br/><span class=\"black\">Tokens Created: </span> " + str(value) + "<hr/>"
+
+    sendEmailAlert("<html> <head> <style> body{ font-family: FrutigerLTStd-Light, Arial, sans-serif;} h1, h2, h3, h4 { color:#e87722; font-weight: bold; text-rendering: optimizeLegibility; margin: 0 0 24px 0; } .black{ color:#333333; }.light{ color:#8c8c8c; font-size: 10px; } </style> </head> <body> <br/> <h1><span class=\"black\">Server:</span> " + casshost + "<br/><span class=\"black\">Cassandra Keyspace: </span> " + str(keyspace) + "<br/><span class=\"black\">OAuth_Tokens Rows Deleted: </span> " + str(rowsdeleted) + "</h1> " + str(consumer_abuse_table) + "</body> </html>", casshost, "OAuth Token Cleanup")
+    cluster.shutdown()
+
+
 deleteExpiredIDs(args.cassandrahost, args.keyspace, args.username, args.password)
