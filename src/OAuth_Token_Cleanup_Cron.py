68cb7cd0220df9b8b51d18a4c17a9ef4b09ddb08
Mono Chong <yusha9788@gmail.com>
2024-06-25 11:08:04 +0800

    convert send email to option

--- src/OAuth_Token_Cleanup_Cron.py
+++ src/OAuth_Token_Cleanup_Cron.py
@@ -30,8 +30,8 @@ parser.add_argument('cassandrahost', type=str, help='hostname of one cassandra c
 parser.add_argument('keyspace', type=str, help='keyspace for the token deletes')
 parser.add_argument('username', type=str, help='cassandra username')
 parser.add_argument('password', type=str, help='cassandra password')
-parser.add_argument('email', type=str, help='email address to notify of token cleanup')
-parser.add_argument('smtpserver', type=str, help='SMTP relay server to use to send notification email')
+parser.add_argument('--email', type=str, help='email address to notify of token cleanup', default=None)
+parser.add_argument('--smtpserver', type=str, help='SMTP relay server to use to send notification email', default=None)
 parser.add_argument('--ssl', action='store_true', default=False, help='(Default false) use SSL for connections to cassandra')
 parser.add_argument('--ca', type=str, help='If using SSL, provide a path to the truststore as a PEM')
 
@@ -51,15 +51,16 @@ else:
 
 
 def sendEmailAlert(BODY, dbhost, subject):
-    msg = MIMEText(BODY, 'html')
-    msg['Subject'] = dbhost + " " + subject
-    sender = dbhost + " OAuth Token Cleanup Script"
-    recipients = [args.email]
-    msg['From'] = sender
-    msg['To'] = ", ".join(recipients)
-    server = smtplib.SMTP(args.smtpserver)
-    server.sendmail(sender, recipients, msg.as_string())
-    server.quit()
+    if args.email and args.smtpserver:
+        msg = MIMEText(BODY, 'html')
+        msg['Subject'] = dbhost + " " + subject
+        sender = dbhost + " OAuth Token Cleanup Script"
+        recipients = [args.email]
+        msg['From'] = sender
+        msg['To'] = ", ".join(recipients)
+        server = smtplib.SMTP(args.smtpserver)
+        server.sendmail(sender, recipients, msg.as_string())
+        server.quit()
 
 
 def deleteExpiredIDs(host, keyspace, user, password):
@@ -69,6 +70,7 @@ def deleteExpiredIDs(host, keyspace, user, password):
     
     # Get current time in UTC
     current_time = datetime.datetime.now(pytz.utc)
+    exec_time = current_time.strftime("%Y-%m-%d %H:%M:%S %Z")
 
     # Count tokens before deletion
     count_before = session.execute("SELECT COUNT(*) FROM oauth2_tokens").one()[0]
@@ -115,7 +117,7 @@ def deleteExpiredIDs(host, keyspace, user, password):
                                     "<br/><span class=\"black\">Consumer Name: </span> " + offending_consumer_username + \
                                     "<br/><span class=\"black\">Tokens Created: </span> " + str(value) + "<hr/>"
 
-    sendEmailAlert("<html> <head> <style> body{ font-family: FrutigerLTStd-Light, Arial, sans-serif;} h1, h2, h3, h4 { color:#e87722; font-weight: bold; text-rendering: optimizeLegibility; margin: 0 0 24px 0; } .black{ color:#333333; }.light{ color:#8c8c8c; font-size: 10px; } </style> </head> <body> <br/> <h1><span class=\"black\">Server:</span> " + casshost + "<br/><span class=\"black\">Cassandra Keyspace: </span> " + str(keyspace) + "<br/><span class=\"black\">OAuth_Tokens Rows Deleted: </span> " + str(rowsdeleted) + "</h1> " + str(consumer_abuse_table) + "</body> </html>", casshost, "OAuth Token Cleanup")
+    sendEmailAlert("<html> <head> <style> body{ font-family: FrutigerLTStd-Light, Arial, sans-serif;} h1, h2, h3, h4 { color:#e87722; font-weight: bold; text-rendering: optimizeLegibility; margin: 0 0 24px 0; } .black{ color:#333333; }.light{ color:#8c8c8c; font-size: 10px; } </style> </head> <body> <br/> <h1><span class=\"black\">Server:</span> " + casshost + "<br/><span class=\"black\">Cassandra Keyspace: </span> " + str(keyspace) + "<br/><span class=\"black\">OAuth_Tokens Rows Deleted: </span> " + str(rowsdeleted) + "</h1> " + str(consumer_abuse_table) + "</body> </html>", casshost, "OAuth Token Cleanup", exec_time)
     cluster.shutdown()
 
 
